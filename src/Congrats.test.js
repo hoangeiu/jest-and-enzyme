4b6c08320e85b121aeaa8e73ee69c61302416154
hoangeiu <leminhhoangk3@gmail.com>
2021-03-15 09:47:06 +0700

    Extract enzyme setup to setupTests.js

--- src/Congrats.test.js
+++ src/Congrats.test.js
@@ -1,11 +1,8 @@
 import React from "react";
-import Enzyme, { shallow } from "enzyme";
-import EnzymeAdapter from "@wojtekmaj/enzyme-adapter-react-17";
+import { shallow } from "enzyme";
 import Congrats from "./Congrats";
 
 import { findByTestAttr, checkProp } from "../test/testUtils";
-
-Enzyme.configure({ adapter: new EnzymeAdapter() });
 
 const defaultProps = { success: false };
 

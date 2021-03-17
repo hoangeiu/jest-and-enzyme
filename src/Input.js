624d16ec43777bdfba537b4f299d8c0822e0b2df
hoangeiu <leminhhoangk3@gmail.com>
2021-03-17 15:49:56 +0700

    Set up Input component and storeFactory test util

---
+++ src/Input.js
@@ -0,0 +1,19 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+
+const Input = (props) => {
+  return (
+    <div>
+      <button />
+    </div>
+  );
+};
+
+Input.propTypes = {};
+
+const mapStateToProps = (state) => {
+  return {};
+};
+
+export default connect(mapStateToProps)(Input);

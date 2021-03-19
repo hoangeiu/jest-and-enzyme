31ea195f07141e316052b68636361c1399a08c1e
hoangeiu <leminhhoangk3@gmail.com>
2021-03-19 10:16:20 +0700

    Add Redux props tests to Input.test.js and change Input component to class comp

--- src/Input.js
+++ src/Input.js
@@ -1,31 +1,34 @@
-import React from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 
-const Input = (props) => {
-  const { success } = props;
+import { actionTypes, guessWord } from "./actions";
 
-  const contents = success ? null : (
-    <form className="form-inline">
-      <input
-        data-test="input-box"
-        className="mb-2 mx-sm-3"
-        id="word-guess"
-        type="text"
-        placeholder="enter guess"
-      />
-      <button
-        data-test="submit-button"
-        type="submit"
-        className="btn btn-primary mb-2"
-      >
-        Submit
-      </button>
-    </form>
-  );
+class Input extends Component {
+  render() {
+    const { success } = this.props;
+    const contents = success ? null : (
+      <form className="form-inline">
+        <input
+          data-test="input-box"
+          className="mb-2 mx-sm-3"
+          id="word-guess"
+          type="text"
+          placeholder="enter guess"
+        />
+        <button
+          data-test="submit-button"
+          type="submit"
+          className="btn btn-primary mb-2"
+        >
+          Submit
+        </button>
+      </form>
+    );
 
-  return <div data-test="component-input">{contents}</div>;
-};
+    return <div data-test="component-input">{contents}</div>;
+  }
+}
 
 Input.propTypes = {};
 
@@ -33,4 +36,10 @@ const mapStateToProps = ({ success }) => {
   return { success };
 };
 
-export default connect(mapStateToProps)(Input);
+const mapDispatchToProps = (dispatch) => {
+  return {
+    guessWord: (guessedWord) => dispatch(guessWord(guessedWord)),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Input);
